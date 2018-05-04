fcbef070f7dab16af6f6dd84fb37554101a0994a
Matthew Day <matt@matthewday.net>
2018-05-04 10:33:25 -0400

    Remove duplicate files

--- src/components/test/recipe_form.test 2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import CreateRecipeForm from './recipe_form';
-
-describe('<CreateRecipeForm />', () => {
-    it('Renders without crashing', () => {
-        shallow(<CreateRecipeForm />);
-    });
-});
