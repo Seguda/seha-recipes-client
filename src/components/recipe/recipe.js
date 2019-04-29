0df6f6c0b52b61581824becae57d38a33dfb5b20
Seha Gulnar Dalay <sehagulnar@gmail.com>
2019-04-28 22:30:05 -0400

    Added click event for pagination.

--- src/components/recipe/recipe.js
+++ src/components/recipe/recipe.js
@@ -1,92 +1,104 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
+import { setRecipe } from "../../actions";
+import { Link } from "react-router-dom";
+import "./recipes.css";
 import { API_BASE_URL } from "../../config";
-import "./recipe.css";
 
-class Recipe extends React.Component {
+class Recipes extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      steps: {}
+      showRecipe: false,
+      recipe: [],
+      text: "",
+      currentPage: 1,
+      recipesPerPage: 6
     };
+    this.setRecipe = this.setRecipe.bind(this);
+    this.closeRecipe = this.closeRecipe.bind(this);
+    this.searchRecipe = this.searchRecipe.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(event) {
+    this.setState({
+      currentPage: Number(event.target.id)
+    });
   }
 
-  componentDidMount() {
-    const {
-      match: { params }
-    } = this.props;
+  setRecipe(recipe) {
+    this.props.dispatch(setRecipe(recipe));
+  }
+
+  closeRecipe() {
     this.setState({
-      steps: { ...this.props.recipe }
+      showRecipe: false
+    });
+  }
+
+  searchRecipe(text) {
+    this.setState({
+      text
     });
   }
 
   render() {
-    var props = this.props.recipe
-      ? this.props
-      : {
-          recipe: {
-            name: "",
-            author: "",
-            ethnicity: "",
-            type: "",
-            servings: 0,
-            ingredients: "",
-            directions: "",
-            downloadUrl: ""
-          }
-        };
-    // console.log(props.recipe);
-    var className =
-      "recipeListItem " + (props.display === "block" ? "block" : "hidden");
-    if (props.recipe.downloadUrl) {
-      var image = `/${props.recipe.downloadUrl}`;
-    } else {
-      var image = `/images/default.jpg`;
+    const recipes = this.props.recipes.map((recipe, index) => {
+      console.log(recipe, recipe.downloadUrl);
+
+      let _param = `/recipes/${recipe.name.toLowerCase()}`;
+      return (
+        <li
+          className="recipeimg"
+          key={index}
+          onClick={() => this.setRecipe(recipe)}
+        >
+          <Link to={{ pathname: _param }}>
+            <img src={`${API_BASE_URL + "/" + recipe.downloadUrl}`} alt="" />
+            <p> {recipe.name} </p>
+            <p> {recipe.type} </p>
+          </Link>
+        </li>
+      );
+    });
+    const { currentPage, recipesPerPage } = this.state;
+    // Logic for displaying current recipes
+    const indexOfLastRecipe = currentPage * recipesPerPage;
+    const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
+    const currentRecipe = recipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
+    // Logic for displaying page numbers
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(recipes.length / recipesPerPage); i++) {
+      pageNumbers.push(i);
     }
 
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <li
+          key={number}
+          id={number}
+          onClick={(e) => this.handleClick(e)}
+        >
+          {number}
+        </li>
+      );
+    });
     return (
-      <div className="singlerecipe">
-        <div className="wrap">
-          <div className="nimg">
-            <img src={API_BASE_URL + image} alt="visual" />
-          </div>
-          <div className="recipetitle">{this.state.steps.name}</div>
-          <ul>
-            <li>
-              <span>Author:</span>
-              {this.state.steps.author}
-            </li>
-            <li>
-              <span>Type:</span>
-              {this.state.steps.type}
-            </li>
-            <li>
-              <span>Ethnicity:</span>
-              {this.state.steps.ethnicity}
-            </li>
-            <li>
-              <span>Servings:</span>
-              {this.state.steps.servings}
-            </li>
-            <li>
-              <span>Ingredients:</span>
-              {this.state.steps.ingredients}
-            </li>
-            <li>
-              <span>Directions:</span>
-              {this.state.steps.directions}
-            </li>
-          </ul>
-        </div>
+      <div className="wrap">
+        <h2>Pure Joy Below</h2>
+        <ul className="recipelist">{recipes}</ul>
+        <ul id="page-numbers">
+          {renderPageNumbers}
+        </ul>
       </div>
     );
   }
 }
 
-const mapStateToProps = function(state) {
+const mapStateToProps = function (state) {
   return {
-    recipe: state.recipeReducer.recipe
+    recipes: state.recipeReducer.recipes
   };
 };
 
-export default connect(mapStateToProps)(Recipe);
+export default connect(mapStateToProps)(Recipes);
